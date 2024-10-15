d6109d0c4511a66a8c7b578ab520951165877cee
Cerrer <144385315+Kocterok@users.noreply.github.com>
2024-10-15 23:17:41 +0300

    views
    HttpResponcse начло (тестим)

--- DANCE/DANCE/settings.py
+++ DANCE/DANCE/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'appDANCE'
+    'appDANCE',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DANCE/appDANCE/views.py
+++ DANCE/appDANCE/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, world. You're at the appDANCE index.")
+    # return render(request, 'appDANCE/index.html')
+def dance(request):
+    return HttpResponse("<H4/>Hello, dances<H4/>")
