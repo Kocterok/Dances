e8ab88097bd236518e18aefbd713375754666396
Cerrer <144385315+Kocterok@users.noreply.github.com>
2024-10-16 08:48:32 +0300

    HTML
    работа с HTML

--- DANCE/appDANCE/urls.py
+++ DANCE/appDANCE/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dance', views.dance, name='dance'),
+    path('about', views.about, name='about'),
 ]
--- DANCE/appDANCE/views.py
+++ DANCE/appDANCE/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world. You're at the appDANCE index.")
-    # return render(request, 'appDANCE/index.html')
+    return render(request, 'appDANCE/index.html')
 def dance(request):
-    return HttpResponse("<H4/>Hello, dances<H4/>")
+    return render(request, 'appDANCE/dance.html')
+
+def about(request):
+    return render(request, 'appDANCE/about.html')
